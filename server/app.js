b351ad8be697a05cfb4ff568ee6da4f1c08135db
Thomas Evans <thomasevans@Thomass-MBP.attlocal.net>
2021-07-19 17:20:23 -0400

    added more styling

--- server/app.js
+++ server/app.js
@@ -5,12 +5,6 @@ const server = app.listen(8080);
 
 const knex = require('knex')({
     client: 'mysql2',
-    // connection: {
-    //     host: process.env.MYSQL_HOST,
-    //     user: process.env.MYSQL_USER,
-    //     database: process.env.MYSQL_DATABASE,
-    //     password: process.env.MYSQL_PASSWORD
-    // }
     connection: {
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
