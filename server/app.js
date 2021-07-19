f8f92a9177efcd3f9b526b91d1a1614c208a8403
Thomas Evans <thomasevans@Thomass-MBP.attlocal.net>
2021-07-19 17:19:04 -0400

    added more styling

--- server/app.js
+++ server/app.js
@@ -12,10 +12,10 @@ const knex = require('knex')({
     //     password: process.env.MYSQL_PASSWORD
     // }
     connection: {
-        host: 'localhost',
-        user: 'root',
-        database: 'chat-app',
-        password: 'nodecomplete'
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        database: process.env.MYSQL_DATABASE,
+        password: process.env.MYSQL_PASSWORD,
     }
 })
 
